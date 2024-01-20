1a84a30cf8b52bf7144c9675c66fb7057eec5ae0
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-01-20 14:30:13 +0000

    v2 initial commit

---
+++ v2-hex/gateway-omg/block2-notes.cpp
@@ -0,0 +1,112 @@
+// ZgatewayLORA
+
+
+// void LORAtoMQTT() {
+// ...
+//    // Create packet and reserve null terminator space
+// --- cut --- and adapt ---
+
+    // ### EASY HAN LORA ####
+
+    byte packet[packetSize + 1];
+    boolean binary = false;
+    
+    uint8_t polySize = CHA_CHA_POLY_TAG_SIZE +
+                 CHA_CHA_POLY_IV_SIZE;
+
+    if (packetSize > polySize & packetSize < 200)
+    {
+      // ### if packet size is Cha Cha ###
+
+      byte rxArray[packetSize];
+
+      for (int i = 0; i < packetSize; i++) {
+        rxArray[i] = (char)LoRa.read();
+      }
+
+      // ### CHA_CHA_POLY ###
+
+      byte tag[CHA_CHA_POLY_TAG_SIZE];
+      byte plainText[packetSize - polySize];
+      byte cipherText[packetSize - polySize];
+
+      memcpy(cipherText, rxArray, sizeof(cipherText));
+      memcpy(tag, &rxArray[sizeof(cipherText)], sizeof(tag));
+      memcpy(iv, &rxArray[sizeof(cipherText) +
+                    sizeof(tag)], sizeof(iv));
+
+#     ifdef CHA_CHA_DEBUG
+      Serial.println(" ");
+      Serial.println("rxArray:");  
+
+      for(i=0; i<sizeof(rxArray); i++)
+      {
+        printHex(rxArray[i]);
+      }
+
+      Serial.println(" ");
+      Serial.println("cipherText:");  
+
+      for(i=0; i<sizeof(cipherText); i++)
+      {
+        printHex(cipherText[i]);
+      }
+
+      Serial.println(" ");
+      Serial.println("Tag:");
+
+      for(i=0; i<sizeof(tag); i++)
+      {
+        printHex(tag[i]);
+      } 
+
+      Serial.println(" ");
+      Serial.println("IV:");
+
+      for(i=0; i<sizeof(iv); i++)
+      {
+        printHex(iv[i]);
+      } 
+#     endif
+
+      // decrypt message from cipherText to plainText
+      // output is valid only if result is true
+      bool verify = ChaChaPolyCipher.decrypt(key, iv, auth, cipherText, plainText, tag);
+
+      if (verify)
+      {
+#       ifdef CHA_CHA_DEBUG
+        Serial.println(" ");
+        Serial.println("Decrypted:");  
+        String decrypted = String((char*)plainText);
+        Serial.println(decrypted);
+#       endif
+
+        for (int i = 0; i < sizeof(plainText); i++) {
+          packet[i] = plainText[i];
+        }
+      }
+      else
+      {
+        binary = true;
+      }
+      // ### CHA_CHA_POLY EOF ###
+    // ### end if packet size is cha cha true ###
+    }
+    else
+    {
+      for (int i = 0; i < packetSize; i++) {
+        packet[i] = (char)LoRa.read();
+
+        if (packet[i] < 32 || packet[i] > 127)
+          binary = true;
+      }
+    }
+    // ### EASY HAN LORA EOF ####
+
+
+// --- cut ---
+// // Terminate with a null character in case we have a string
+
+// EOF
+
---
+++ v2-hex/node-modbus/src/han.h
@@ -0,0 +1,66 @@
+// han.h
+
+uint8_t hanCNT = 0;
+uint8_t hanCFG = 99;
+uint8_t hanEB = 99;
+uint16_t hanERR = 0;
+uint16_t hanDTT = 0;
+
+// Clock 01
+uint16_t hanYY = 0;
+uint8_t hanMT = 0;
+uint8_t hanDD = 0;
+uint8_t hanWD = 0;
+uint8_t hanHH = 0;
+uint8_t hanMM = 0;
+uint8_t hanSS = 0;
+
+// Voltage Current 6C
+// mono
+uint16_t hanVL1 = 0;
+uint16_t hanCL1 = 0;
+// tri
+uint16_t hanVL2 = 0;
+uint16_t hanCL2 = 0;
+uint16_t hanVL3 = 0;
+uint16_t hanCL3 = 0;
+uint16_t hanCLT = 0;
+
+// Total Energy T (kWh) 26
+uint32_t hanTET1 = 0;
+uint32_t hanTET2 = 0;
+uint32_t hanTET3 = 0;
+
+// Total Energy (kWh) 16
+uint32_t hanTEI = 0;
+uint32_t hanTEE = 0;
+
+
+// Active Power Import/Export 73
+// tri
+uint32_t hanAPI1 = 0;
+uint32_t hanAPE1 = 0;
+uint32_t hanAPI2 = 0;
+uint32_t hanAPE2 = 0;
+uint32_t hanAPI3 = 0;
+uint32_t hanAPE3 = 0;
+// mono / tri total. (79)
+uint32_t hanAPI = 0;
+uint32_t hanAPE = 0;
+
+// Power Factor (7B) / Frequency (7F)
+uint16_t hanPF = 0;
+uint16_t hanPF1 = 0;
+uint16_t hanPF2 = 0;
+uint16_t hanPF3 = 0;
+uint16_t hanFreq = 0;
+
+// EOF
+
+
+
+
+
+
+
+
